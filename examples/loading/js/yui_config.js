18c81aad68173333be73a8a8b0098df7637d1df9
Evan Goer <evan@goer.org>
2012-04-22 21:25:17 -0700

    Added comments about relative paths.

--- examples/loading/js/yui_config.js
+++ examples/loading/js/yui_config.js
@@ -1,6 +1,7 @@
 var YUI_config = {
     groups: {
         'local-modules': {
+            // NOTE: On a local filesystem, change to './js/local-modules/'
             base: '/js/local-modules/',
             modules: {
                 'reptiles-core': {
